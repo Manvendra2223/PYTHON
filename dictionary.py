12b9256da96bc54aed618c7b324fd477a83be5db
Manvendra Singh <156326804+Manvendra2223@users.noreply.github.com>
2025-11-09 21:50:50 +0530

    second commit

--- dictionary.py
+++ dictionary.py
@@ -20,5 +20,46 @@ for chai in chai_types :
         if "Masala" in chai_types :
             print("Yes, Masala is present")
             print(len(chai_types))
-            
-            
+        else :
+            print("No, Masala is not present")
+chai_types["Kashmiri"] = "Rich"
+print(chai_types)
+print(len(chai_types))
+chai_types.pop("Adrak")
+print(chai_types)
+print(len(chai_types))
+chai_types.clear()
+print(chai_types)
+chai_types = {"Masala":"Spicy", "Adrak":"Ginger", "Elaichi":"Cardamom"}
+print(chai_types)
+chai_types["Earl Grey"] = "Citrus"
+print(chai_types)
+chai_types["Masala"] = "Very Spicy"
+print(chai_types)
+print(len(chai_types))
+chai_types.pop("Elaichi")
+print(chai_types)
+del chai_types["Adrak"]
+print(chai_types)
+chai_types_copy = chai_types.copy()
+[print(chai_types_copy)]
+tea_shop = {
+    "chai": {"Masala":"Spicy", "Adrak":"Ginger", "Elaichi":"Cardamom"},
+    "snacks": ["Samosa", "Pakora", "Chaat"],
+    "location": "Delhi",
+}
+print(tea_shop)
+print(tea_shop["chai"])
+print(tea_shop["snacks"], tea_shop["location"])
+squared_numbers = {1:1, 2:4, 3:9, 4:16, 5:25}
+print(squared_numbers)
+print(squared_numbers[3])
+print(squared_numbers.get(4))
+print(squared_numbers.get(6, "Not Found"))
+keys = ["Masala","Ginger","Adrak"]
+default_value = "Available"
+new_dict = dict.fromkeys(keys, default_value)
+print(new_dict)
+new_dict = dict.fromkeys(keys, keys)
+print(new_dict)
+
