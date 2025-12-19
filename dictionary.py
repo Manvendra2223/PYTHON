fd82d37e04f5a3bb80ce16de82372099384f9767
Manvendra Singh <156326804+Manvendra2223@users.noreply.github.com>
2025-12-19 21:50:13 +0530

    initial step

--- dictionary.py
+++ dictionary.py
@@ -62,4 +62,6 @@ new_dict = dict.fromkeys(keys, default_value)
 print(new_dict)
 new_dict = dict.fromkeys(keys, keys)
 print(new_dict)
+print(dict.fromkeys(range(1,6)))
+print(dict.fromkeys("Chai"))
 
