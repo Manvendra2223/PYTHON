707cc39d33220ea9d24375fffe664c5169a30a3c
Manvendra Singh <156326804+Manvendra2223@users.noreply.github.com>
2025-12-26 20:17:35 +0530

    third commit

---
+++ Conditional/question3.py
@@ -0,0 +1,19 @@
+
+# Grade Calculator:
+
+score = -2
+
+if score > 100 or score < 0:
+    print("Invalid score")
+    exit()
+if score >= 90:
+        grade = "A"
+elif score >= 80:
+        grade = "B"
+elif score >= 70:
+        grade = "C"
+elif score >= 60:
+        grade = "D"
+else:
+        grade = "F"
+print("Your grade is:", grade)
