cee2331fa6c3cd62c06a7def3ee708d39839e6d3
Manvendra Singh <156326804+Manvendra2223@users.noreply.github.com>
2026-01-01 19:01:22 +0530

    initial commit

---
+++ Conditional/question4.py
@@ -0,0 +1,15 @@
+
+## Fruit Ripeness Checker
+
+fruit = "Banana"
+color = "Black"
+
+if fruit == "Banana":
+    if color == "Green":
+        print("Unripe")
+    elif color == "Yellow":
+        print("Ripe")
+    elif color == "Brown":
+        print("Overripe")
+    else:
+        print("Unknown ripeness")
---
+++ Conditional/question5.py
@@ -0,0 +1,12 @@
+
+## Weather Activity Suggestion:
+
+weather ="Sunny"
+
+if weather == "Sunny":
+    activity = "Go for a walk in the park."
+elif weather == "Rainy":
+    activity = "Stay indoors and read a book."
+elif weather == "Cloudy":
+    activity = "Visit a museum." 
+print(activity)   
