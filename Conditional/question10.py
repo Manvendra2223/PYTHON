6fe7b55e56bac0bc80aece6b358c4f370149bff4
Manvendra Singh <156326804+Manvendra2223@users.noreply.github.com>
2026-01-02 12:23:49 +0530

    initial commit

---
+++ Conditional/question10.py
@@ -0,0 +1,13 @@
+
+## Pet Food Recommendation:
+
+age = 24
+
+if age < 1:
+    food = "puppy food"
+elif age < 7:
+    food = "adult dog food"
+else:
+    food = "senior dog food"
+    
+print("Recommended Food:", food)
