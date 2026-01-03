7328550497ad1b285f0aea3f171f5869be65d86c
Manvendra Singh <156326804+Manvendra2223@users.noreply.github.com>
2026-01-03 21:09:31 +0530

    Exponential Backoff

--- loop_problems/question10.py
+++ loop_problems/question10.py
@@ -5,3 +5,12 @@
 
 import time
 
+wait_time = 1  # initial wait time in seconds
+max_retries = 5  # maximum number of retries
+attempts = 0  # counter for attempts
+
+while attempts < max_retries:
+    print("Attempts",attempts+ 1, "wait_time", wait_time, "seconds")
+    time.sleep(wait_time)
+    wait_time *= 2
+    attempts += 1
