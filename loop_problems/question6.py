c303190b92b72808eb3094f4652ce953090958f5
Manvendra Singh <156326804+Manvendra2223@users.noreply.github.com>
2026-01-03 11:33:49 +0530

    initial commit

---
+++ loop_problems/question6.py
@@ -0,0 +1,16 @@
+
+
+## Factorial calculator using  while loop:
+
+number = 23
+factorial = 1
+
+while number > 0:
+   # factorial = factorial * number
+    factorial *= number # shorthand for factorial = factorial * number
+    number = number - 1
+
+print("Factorial value of the given number is:", factorial)
+    
+
+
