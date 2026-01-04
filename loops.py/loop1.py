b5e15ff80a21ddcfcf45396b2254b56b2bbe5a6f
Manvendra Singh <156326804+Manvendra2223@users.noreply.github.com>
2026-01-04 18:39:43 +0530

    iteration tools

---
+++ loops.py/loop1.py
@@ -0,0 +1,50 @@
+
+   ## iteration tool
+   ## (for, while,comprehension)
+ 
+   ## using  iter() and next()
+   ##items = [1,2,3]
+   ##iterator = iter(items)
+   ##print(next(iterator))
+   ##print(next(iterator))
+   ##print(next(iterator))
+   
+   ## iterable objects (lists,file)
+   ## also goes to next() item
+   
+  # f.readline() #read line by line # also an iterable object
+  
+  # stop iteration error when no more lines to read
+  
+  # for loop
+  # for line in open('data.txt'): ## also we can use readlines()
+  #     print(line,end="") ## to avoid double new line
+  
+  # while loop
+  # f = open('data.txt')
+  # line = f.readline()
+  #if not line:break
+  #print(line,end="")
+  
+  
+  #test = "hitesh"
+  #if not test:
+      #print("Empty String") # output is nothing
+      
+  #test =""
+  #if not test:
+      #print("Empty String") # output is empty string
+      
+      
+   # myList = [1,2,3,4,5]
+   # I = iter(myList)
+   # I
+   
+   # iter objects and default in file
+   # but not in list
+   
+   #dictonary are iterable objects
+   # range objects are iterable objects
+   
+    
+   
