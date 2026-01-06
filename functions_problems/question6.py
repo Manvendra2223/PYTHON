b3bd2ce665115ed73d0848793187b631d53262c9
Manvendra Singh <156326804+Manvendra2223@users.noreply.github.com>
2026-01-06 11:20:57 +0530

    Lambda Function

--- functions_problems/question6.py
+++ functions_problems/question6.py
@@ -1,6 +1,6 @@
 
-## Lambda Function 
-## Create a lambda function to compute the cube of a number .
+### Lambda Function 
+###  Create a lambda function to compute the cube of a number .
 
 cube = lambda x: x ** 3  # defining a lambda function that computes the cube of x
 print(cube(4))  # calling the lambda function with argument 4 and printing the result
