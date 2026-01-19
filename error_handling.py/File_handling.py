339b05f793b9da39ac192abceeef3e4da16a9387
Manvendra Singh <156326804+Manvendra2223@users.noreply.github.com>
2026-01-19 09:44:51 +0530

    File handling

---
+++ error_handling.py/File_handling.py
@@ -0,0 +1,28 @@
+
+ ## File Handling 
+ 
+ 
+ 
+# Open a file named "youtube.txt" in write mode ('w')
+# If the file does not exist, Python will create it automatically
+file = open("youtube.txt", 'w')
+
+try:
+    # Write some text inside the file
+    file.write('chai aur code')
+
+# "finally" block always runs, even if an error happens above
+finally:
+    # Close the file to save changes and free memory
+    file.close()
+
+
+# Another and better way to work with files is using "with"
+# "with" automatically closes the file for us, so we don’t need file.close()
+with open('youtube.txt', 'w') as file:
+    # This will overwrite the previous content because mode is 'w'
+    file.write('manish is cute')
+
+    
+
+    
--- error_handling.py/youtube_manager.py
+++ error_handling.py/youtube_manager.py
@@ -1,28 +1,44 @@
 
- ## File Handling 
- 
- 
- 
-# Open a file named "youtube.txt" in write mode ('w')
-# If the file does not exist, Python will create it automatically
-file = open("youtube.txt", 'w')
-
-try:
-    # Write some text inside the file
-    file.write('chai aur code')
-
-# "finally" block always runs, even if an error happens above
-finally:
-    # Close the file to save changes and free memory
-    file.close()
+def list_all_videos(videos):
+    pass
 
 
-# Another and better way to work with files is using "with"
-# "with" automatically closes the file for us, so we don’t need file.close()
-with open('youtube.txt', 'w') as file:
-    # This will overwrite the previous content because mode is 'w'
-    file.write('manish is cute')
+def add_video(videos):
+    pass
 
+def update_video(videos):
+    pass
+
+def delete_video(videos):
+    pass
     
-
     
+videos = []
+
+
+while True:  
+    print("\n Youtube Manager | choose an option ")
+    print("1. List all youtube videos")
+    print("2. Add a yotube video")
+    print("3. Update a youtube video details ")
+    print("4. Delete a youtube video")
+    print("5. Exit the app ")
+    choice = input(" Enter your choice ")
+    
+    match choice: 
+        case '1':
+            list_all_videos(videos)
+        case'2':
+            add_video(videos)
+        case'3':
+            update_video(videos)
+        case '4':
+            delete_video(videos)
+        case'5':
+            break
+        case _:
+                print()
+                
+                
+                
+            
